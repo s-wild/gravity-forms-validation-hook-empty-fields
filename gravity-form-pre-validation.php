2ae1f634675b6813f1db420ece406099f4769ce4
Simon Wild <simonwild@protonmail.com>
2017-08-14 10:04:28 -0400

    Update gravity-form-pre-validation.php

--- gravity-form-pre-validation.php
+++ gravity-form-pre-validation.php
@@ -7,23 +7,23 @@ add_filter( 'gform_validation_2', 'check_for_age_group' );
 function check_for_age_group( $validation_result ) {
 		$form = $validation_result['form'];
 		if(empty($_POST["input_31_1"]) && empty($_POST["input_12_1"])){
-			// set the form validation to false
+			// Set the form validation to false
 			$validation_result['is_valid'] = false;
 			//finding Field with ID of 1 and marking it as failed validation
 			foreach( $form['fields'] as &$field ) {
 					if ( $field->id == '31') {
 						$field->failed_validation = true;
-						$field->validation_message = 'Oops! Please select either 55+ Living or All Ages.';
+						$field->validation_message = 'Oops! Please select either of these options.';
 						continue;
 					}
 					if ($field->id == '12'){
 						$field->failed_validation = true;
-						$field->validation_message = 'Oops! Please select either 55+ Living or All Ages.';
+						$field->validation_message = 'Oops! Please select either of these options.';
 						continue;
 					}
 			}
 		}
-		//Assign modified $form object back to the validation result
+		// Assign modified $form object back to the validation result
 		$validation_result['form'] = $form;
 		return $validation_result;
 }
